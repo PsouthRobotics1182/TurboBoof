f1003fc6a6c9acbacd7f98a1c8734996b6dbf10e
Robotics <beindera7091@parkwayschools.net>
2018-03-22 18:06:35 -0500

    quad servo lift

--- TeamCode/src/main/java/lib/fine/systems/Flipper.java
+++ TeamCode/src/main/java/lib/fine/systems/Flipper.java
@@ -1,6 +1,7 @@
 package lib.fine.systems;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -12,28 +13,42 @@ import lib.fine.core.FineMotor;
  */
 
 public class Flipper {
-    private FineMotor lift;
-    private Servo servo;
+    //private FineMotor lift;
+    private Servo flipper;
+    private CRServo topLeft;
+    private CRServo topRight;
+    private CRServo bottomRight;
+    private CRServo bottomLeft;
+
     private LinearOpMode opMode;
 
     public Flipper(LinearOpMode opMode) {
-        lift = new FineMotor(opMode, "leftMotor");
-        servo = opMode.hardwareMap.get(Servo.class, "flip");
+        //lift = new FineMotor(opMode, "leftMotor");
+        flipper = opMode.hardwareMap.get(Servo.class, "flip");
 
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lift.setDirection(DcMotorSimple.Direction.FORWARD);
+        topLeft = opMode.hardwareMap.get(CRServo.class, "topLeft");
+        topRight = opMode.hardwareMap.get(CRServo.class, "topRight");
+        bottomRight = opMode.hardwareMap.get(CRServo.class, "bottomRight");
+        bottomLeft = opMode.hardwareMap.get(CRServo.class, "bottomLeft");
+
+
+        //lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //lift.setDirection(DcMotorSimple.Direction.FORWARD);
         this.opMode = opMode;
         flip(1);
 
     }
     public void lift(double power) {
-        lift.setPower(power);
+        topLeft.setPower(power);
+        topRight.setPower(-power);
+        bottomLeft.setPower(power);
+        bottomRight.setPower(-power);
     }
 
     public void flip (double flipiness) {
-        servo.setPosition(flipiness);
+        flipper.setPosition(flipiness);
     }
     public void addTelemetry() {
-        lift.addTelemetry();
+        //lift.addTelemetry();
     }
 }
